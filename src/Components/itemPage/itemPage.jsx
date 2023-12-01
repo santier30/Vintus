3f7a35abb5bd6c62589db04377ade3b9e0d18ee2
Santiago Hryb <santihryyb@gmail.com>
2023-12-01 20:19:41 -0300

    minor fix

--- src/Components/itemPage/itemPage.jsx
+++ src/Components/itemPage/itemPage.jsx
@@ -37,41 +37,43 @@ try {
     useEffect(() => {
       getWine()
     }, [getWine])
+if(image){
+  return(flag?<main className="itemPage">
+  <section className="itemSec">
+    <article className="itemImg" >
+      {image && <ReactImageZoom {...props}/>}
+      <img src={image} alt={name}  style={{height: '110px',width:' 110px'}}/>
+    </article>
+    <article className="itemArt">
+      <h2 className="under">{name + ` (${wine.stock})`}</h2>
+      <h3 className="under" >CONTENIDO</h3>
+      <p className="under">{wine.long_description}</p>
 
-    return(flag?<main className="itemPage">
-      <section className="itemSec">
-        <article className="itemImg" >
-          {image && <ReactImageZoom {...props}/>}
-          <img src={image} alt={name}  style={{height: '110px',width:' 110px'}}/>
-        </article>
-        <article className="itemArt">
-          <h2 className="under">{name + ` (${wine.stock})`}</h2>
-          <h3 className="under" >CONTENIDO</h3>
-          <p className="under">{wine.long_description}</p>
+      <div className="itemAdd">
+        <p>Precio unitario</p>
+        <span className="price">{'$ ' +wine.price}</span>
 
-          <div className="itemAdd">
-            <p>Precio unitario</p>
-            <span className="price">{'$ ' +wine.price}</span>
+        <p>Seleccione Cantidad</p>
 
-            <p>Seleccione Cantidad</p>
+        <div className="item-buttons">
+        <button onClick={()=>{setQuantity(()=>quantity===1?1:quantity-1)}} >-</button>
+        <div>{quantity}</div>
+        <button onClick={()=>{setQuantity(()=>quantity===wine.stock?quantity:quantity+1)}}>+</button>
 
-            <div className="item-buttons">
-            <button onClick={()=>{setQuantity(()=>quantity===1?1:quantity-1)}} >-</button>
-            <div>{quantity}</div>
-            <button onClick={()=>{setQuantity(()=>quantity===wine.stock?quantity:quantity+1)}}>+</button>
+      </div>
+      <button className="add" onClick={Add}>Agregar al carrito</button>
+      </div>
 
-          </div>
-          <button className="add" onClick={Add}>Agregar al carrito</button>
-          </div>
+    </article>
+    <div className="corner_top1"></div>
+    <div className="corner_botom1"></div>
+    <div className="corner_top2"></div>
+    <div className="corner_botom2"></div>
+  </section>
 
-        </article>
-        <div className="corner_top1"></div>
-        <div className="corner_botom1"></div>
-        <div className="corner_top2"></div>
-        <div className="corner_botom2"></div>
-      </section>
-
-    </main>:<h1>Producto no encontrado</h1>)
+</main>:<h1>Producto no encontrado</h1>)
+}else{return <h1></h1>}
+   
 }
 
 export default ItemPage
